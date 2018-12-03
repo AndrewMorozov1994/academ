87ef8cb110123acbc0ca4466682b9a5e662f6daf
Андрей <089ax@mail.ru>
2018-12-03 04:59:01 +0300

    index

---
+++ source/js/inex-filters.js
@@ -0,0 +1,47 @@
+const filters = document.querySelectorAll('.slider__filter-item');
+const subMenuArray = document.querySelectorAll('.slider__filter-sub-list')
+
+const openSubMenu = (it, value, i) => {
+  const subMenu = it.querySelector('.slider__filter-sub-list');
+  if(subMenu == null) return;
+
+  for (let j = 0; j < subMenuArray.length; j++) {
+    if (j == i) {
+      j++;
+    }
+
+    if (j == subMenuArray.length) break;
+
+      if (subMenuArray[j].classList.contains('slider__filter-sub-list--open')) {
+          subMenuArray[j].classList.remove('slider__filter-sub-list--open');
+      }
+
+  }
+
+  subMenu.classList.toggle('slider__filter-sub-list--open');
+  changeValue(subMenu, value);
+}
+
+const changeValue = (item, value) => {
+  const arrayOfValue = item.querySelectorAll('.slider__filter-sub-item');
+      if (arrayOfValue == null) return;
+
+      arrayOfValue.forEach((its) => {
+        its.addEventListener('click', () => {
+          value.innerText = its.innerText;
+        })
+      })
+}
+
+export const getElem = () => {
+  filters.forEach((it, i) => {
+    it.addEventListener('click', () => {
+      const value = it.querySelector('span');
+      if (value == null) return;
+      openSubMenu(it, value, i);
+
+    })
+
+
+  })
+}
